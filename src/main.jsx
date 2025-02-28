4069e4272024e044f2eb328f7b443b1a49383463
Egglol1 <24egglal@gmail.com>
2025-02-28 13:44:20 -0500

    Integrated Atatus

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import * as atatus from 'atatus-spa';
+atatus.config('17cc1233e06b4200ba58a28178c6bf26').install();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
+
+atatus.notify(new Error('Test Atatus Setup'));
