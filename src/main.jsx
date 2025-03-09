cab2b9d5eee73d4f8da6e73955dc288afc9225a9
Egglol1 <24egglal@gmail.com>
2025-03-09 12:01:46 -0400

    Added service worker

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
-import * as atatus from 'atatus-spa';
-atatus.config('17cc1233e06b4200ba58a28178c6bf26').install();
+// import * as atatus from 'atatus-spa';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+//atatus.config('17cc1233e06b4200ba58a28178c6bf26').install();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+serviceWorkerRegistration.register();
