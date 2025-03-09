706cfa4eb5beeaa0f37f7c8afca547fd98bebcd3
Egglol1 <24egglal@gmail.com>
2025-03-09 12:13:08 -0400

    Changed redirect uri to github pages link

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -6,7 +6,7 @@ const SCOPES = [
   'https://www.googleapis.com/auth/calendar.events.public.readonly',
 ];
 const { CLIENT_SECRET, CLIENT_ID, CALENDAR_ID } = process.env;
-const redirect_uris = ['https://github.com/Egglol1/meet'];
+const redirect_uris = ['https://egglol1.github.io/meet/'];
 
 const oAuth2Client = new google.auth.OAuth2(
   CLIENT_ID,
